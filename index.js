2386d0097f48bed95276b1e3f6b7e3a03cf66847
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-21 17:33:59 +0600

    two middlewares added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+// Set MiddleWares Here 
+app.use(cors()) //----- to share data with client side (localhost:3000)
+app.use(express.json()) //----- to parse the string data we get from client side
+
 app.get('/', (req, res) => {
     res.send("Running My Node server successfully 😃 ")
 })
